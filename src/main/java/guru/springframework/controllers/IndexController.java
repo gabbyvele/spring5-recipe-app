985e7cb1231eac51a2a86fdd569f91174febfee6
gabbyvele <gabbyvele@gmail.com>
2020-04-27 03:46:20 +0200

    Testing

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-
     private final RecipeService recipeService;
 
     public IndexController(RecipeService recipeService) {
